991ae7c72dba8c926cf87f236263b26de2dd840c
Jacob Long <jclong98@gmail.com>
2021-06-10 00:09:12 -0700

    camera warmup

--- take_picture.py
+++ take_picture.py
@@ -1,11 +1,19 @@
+import time
+
 from picamera import PiCamera
 
 camera = PiCamera()
 # camera.resolution = (1024, 768)
 # camera.rotation = 180
 
+
 from datetime import datetime
 
 def take_picture(path):
     print(f"{datetime.now()}: {path}")
+    
+    # Camera warm-up time
+    camera.start_preview()
+    time.sleep(1)
+
     camera.capture(path)
